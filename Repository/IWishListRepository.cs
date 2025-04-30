574132b10c700233eece58b40861a0c1463dc185
erfanslh <erfan.slh@yahoo.com>
2025-04-30 13:54:29 +0200

    feat(wishlist): implement wishlist repository and interface

---
+++ Repository/IWishListRepository.cs
@@ -0,0 +1,14 @@
+﻿using BirthdayApp.Model;
+
+namespace BirthdayApp.Repository
+{
+    public interface IWishListRepository
+    {
+        Task<IEnumerable<WishList>> GetUserWishListAsync(string userId);
+        Task <WishList?> GetByIdAsync(int id);
+        Task<WishList> AddAsync(WishList wishList);
+        Task<WishList?> Remove(WishList wishList);
+        Task<WishList?> Update(WishList wishList);
+        Task SaveChangesAsync();
+    }
+}
